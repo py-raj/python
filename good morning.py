07bd97883c5ad9d0d30f16a1ff37773802b2d37f
py-raj <temberaj17@gmail.com>
2025-02-05 14:01:24 +0530

    Update good morning.py

--- good morning.py
+++ good morning.py
@@ -1,12 +1,14 @@
 import time
 timestamp = int(time.strftime("%H"))
 currenttime = time.strftime('%H:%M:%S')
-if timestamp >= 0 and timestamp < 12:
-  print("Good Morning Sir")
-elif timestamp >= 12 and timestamp <= 16:
-  print("Good Afternoon Sir")
-elif timestamp >= 19:
-  print("Good Night Sir")
-else:
-  print("Good Evening Sir")
+def greetyouser():
+  if timestamp >= 0 and timestamp < 12:
+    print("Good Morning Sir")
+  elif timestamp >= 12 and timestamp <= 16:
+    print("Good Afternoon Sir")
+  elif timestamp >= 19:
+    print("Good Night Sir")
+  else:
+    print("Good Evening Sir")
+greetyouser()    
 print("The current time is", currenttime)
